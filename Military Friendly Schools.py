2a4f486d9eba582be42d2689c7062b4f39005fb6
roblero <mr.roblero@gmail.com>
2020-05-29 15:43:32 -0700

    Working

--- Military Friendly Schools.py
+++ Military Friendly Schools.py
@@ -1,11 +1,10 @@
 #https://www.youtube.com/watch?v=H8wUYz22joM
 # https://www.militaryfriendly.com/schools/?pageresult=2
 import urllib
-import urllib.request
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 import os
-from string import ascii_lowercase
+#from string import ascii_lowercase
 
 """
 #Trying to find button
@@ -42,7 +41,7 @@ for i in range(8):
     soup = make_soup(f"http://www.militaryfriendly.com/schools/?pageresult={page_number}")
     page_number = page_number + 1
     print(make_soup)
-"
+"""
 # make soup function
 def make_soup(one):
     url = Request(one, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
@@ -50,19 +49,22 @@ def make_soup(one):
     soupdata = BeautifulSoup(thepage,"html.parser", )
     return soupdata
 
+# Initiate var
 tabledatasaved= ""
 page_number = 1
 
 # Go through rows and columns pulling data
 for i in range(8):
-    soup = make_soup(f"http://www.militaryfriendly.com/schools/?pageresult={page_number}")
+    soup = make_soup(f"https://www.militaryfriendly.com/schools/?pageresult={page_number}")
+    #print(f"this is the page we're on {page_number}")
     for record in soup.findAll('tr'):
         tabledata = ""
         for data in record.findAll('td'):
             tabledata = tabledata + "," + data.text
         if len(tabledata) != 0:
             tabledatasaved = tabledatasaved.strip() + "\n" + tabledata[1:]
-    page_number = page_number + 1
+    page_number += 1
+
 print(tabledatasaved)
 file = open(os.path.expanduser("military_friendly.csv"),"wb")
 file.write(bytes(tabledatasaved, encoding='ascii', errors='ignore'))
